2ce0586adbfd937638c4feca36dce3049ad145de
suraj <surajdeveloper2@gmail.com>
2025-05-23 21:59:12 +0530

    Added report and other file

---
+++ src/test/java/Runner/CucumberRunner.java
@@ -0,0 +1,9 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/features",glue = {"Steps","Hooks"}, plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},tags = "")
+public class CucumberRunner extends AbstractTestNGCucumberTests {
+}
+
